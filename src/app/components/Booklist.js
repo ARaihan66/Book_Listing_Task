5cd7a915abba80005a0288a3d0a33783ca396f10
Abu Raihan <aburaihancse066@gmail.com>
2024-06-06 13:38:51 +0600

    Task finish

--- src/app/components/Booklist.js
+++ src/app/components/Booklist.js
@@ -1,22 +1,25 @@
+
 import { use } from 'react';
 import axios from 'axios';
 
 export default function BookList({ filter, page }) {
-  const books = use(async () => {
+  const booksPromise = use(async () => {
     const url = `http://192.168.0.157:8000/books?page=${page}&filter=${filter}`;
     const res = await axios.get(url);
     return res.data;
   });
 
-  if (books.error) {
-    return <div>Error fetching books</div>;
+  if (booksPromise.pending) {
+    return <div>Loading...</div>;
+  } else if (booksPromise.error) {
+    return <div>Error: {booksPromise.error.message}</div>;
+  } else {
+    return (
+      <div>
+        {booksPromise.value.map((book) => (
+          <div key={book.id}>{book.title}</div>
+        ))}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      {books.map((book) => (
-        <div key={book.id}>{book.title}</div>
-      ))}
-    </div>
-  );
 }
